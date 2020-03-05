7b782f4ad98f6a7fb726a13e428a201cb9ffeb78
yangzl <youngzil@163.com>
2020-03-05 17:14:16 +0800

    Spring事物传播类型 springBoot配置Redis多数据源

---
+++ quickstart-spring-redis/src/main/java/com/quickstart/springboot/redis/common/RedisKeyExpirationListener.java
@@ -0,0 +1,38 @@
+package com.quickstart.springboot.redis.common;
+
+//import io.lettuce.core.RedisClient;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @author youngzil@163.com
+ * @description TODO
+ * @createTime 2020/2/18 16:34
+ */
+//@Component
+//@Slf4j
+public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
+
+//    @Autowired
+//    private RedisClient redisClient;
+    /**
+     * Creates new {@link MessageListener} for {@code __keyevent@*__:expired} messages.
+     *
+     * @param listenerContainer must not be {@literal null}.
+     */
+    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
+        super(listenerContainer);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        // 用户做自己的业务处理即可,注意message.toString()可以获取失效的key
+        String expiredKey = message.toString();
+        System.out.println("expiredKey=" + expiredKey);
+        if(expiredKey.startsWith("zeus:order")){
+            //TODO
+        }
+    }
+}
