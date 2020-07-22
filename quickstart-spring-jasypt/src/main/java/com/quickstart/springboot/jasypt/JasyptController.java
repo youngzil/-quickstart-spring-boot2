30127f0effab4901a7e1f109d6ddc739443047e2
yangzl <youngzil@163.com>
2020-07-23 00:56:09 +0800

    fix(webflux-netty): webflux-netty功能
    Signed-off-by: yangzl <youngzil@163.com>

--- quickstart-spring-jasypt/src/main/java/com/quickstart/springboot/jasypt/JasyptController.java
+++ quickstart-spring-jasypt/src/main/java/com/quickstart/springboot/jasypt/JasyptController.java
@@ -1,6 +1,8 @@
 package com.quickstart.springboot.jasypt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,15 @@ public class JasyptController {
   @Value("${encryptedv2.property}")
   private String property;
 
+  @Autowired
+  private Environment environment;
+
   @RequestMapping("/get")
   public String get() {
+
+    // 拦截是在getProperty之后，get之前还是密文
+    environment.getProperty("encryptedv2.property");
+
     return property;
   }
 
